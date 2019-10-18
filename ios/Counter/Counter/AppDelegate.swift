d024ed65fbba6f35da89b967b4e5efefa4ddb6f9
Max Alexander <max@ditto.live>
2019-10-18 13:06:33 +0900

    adding counter

---
+++ ios/Counter/Counter/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Counter
+//
+//  Created by Maximilian Alexander on 9/29/19.
+//  Copyright © 2019 Ditto. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
